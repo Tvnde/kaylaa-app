741fee1621ea9e1b90cf1b92a4be61628df8cbd3
vreemdeling <bamgbade_tunde@yahoo.com>
2022-10-12 21:22:19 +0100

    front-end changes

--- src/components/campaigns/Campaign.jsx
+++ src/components/campaigns/Campaign.jsx
@@ -1,16 +1,23 @@
 import { AddOutlined, AssessmentOutlined, FilterListOutlined, SearchOutlined } from '@mui/icons-material'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { useNavigate } from "react-router-dom";
+import { fetchCampaigns } from '../../actions/campaign';
 import Sidebar from '../partials/sidebar/Sidebar'
 
 import './campaign.scss'
 
 const Campaign = () => {
     let navigate = useNavigate()
+    let dispatch = useDispatch()
+    let campaigns = useSelector((state) => state.campaigns)
     let newCampaign = ()=> {
         navigate('/campaigns/new')
     }
+    useEffect(() => {
+        dispatch(fetchCampaigns())
+    }, [dispatch])
   return (
     <div>
         <Sidebar/>
@@ -39,191 +46,50 @@ const Campaign = () => {
                     </div>
                 </div>
                 <div className="table">
+                    <div className="campaign-headers">
+                        <div className="campaign-header" style={{flex: '2.3'}}>Details</div>
+                        <div className="campaign-header" style={{flex: '1'}}>Post</div>
+                        <div className="campaign-header" style={{flex: '1'}}>Views</div>
+                        <div className="campaign-header" style={{flex: '1'}}>Engagements</div>
+                        <div className="campaign-header" style={{flex: '1'}}>Shares</div>
+                        <div className="campaign-header" style={{flex: '1'}}>Status</div>
+                        <div className="campaign-header" style={{flex: '1'}}>End Date</div>
+                        <div className="campaign-header" style={{flex: '1'}}>Actions</div>
+                    </div>
+                    {campaigns.map((campaign) => (
                     <div className="campaign-instance">
-                        <div className="basic-details">
-                            <div className="campaign-title">Instance of Campaign title</div>
-                            <div className="other-basic">
-                                <div className="influencer-basic">7 Influencers</div>
-                                <div className="brand-basic">Brand Company</div>
-                                <div className="time-posted">8:14PM</div>
-                            </div>
+                    <div className="basic-details">
+                        <div className="campaign-title">{campaign.title}</div>
+                        <div className="other-basic">
+                            <div className="influencer-basic">No Influencer Yet</div>
+                            <div className="brand-basic">{campaign.brand}</div>
+                            <div className="time-posted">8:14PM</div>
                         </div>
-                        <div className="campaign-info">
-                            <div className="campaign-posts">
-                                <div className="post-value">16</div>
-                                <div className="post-title">posts</div>
-                            </div>
-                            <div className="campaign-views">
-                                <div className="view-value">1253</div>
-                                <div className="view-title">views</div>
-                            </div>
-                            <div className="campaign-engagement">
-                                <div className="engagement-value">93</div>
-                                <div className="engagement-title">engagements</div>
-                            </div>
-                            <div className="campaign-shares">
-                                <div className="share-value">12</div>
-                                <div className="share-title">shares</div>
-                            </div>
-                            <div className="campaign-status">
-                                Active
-                            </div>
+                    </div>
+                        <div className="campaign-posts">
+                            0
                         </div>
+                        <div className="campaign-views">
+                            0
+                        </div>
+                        <div className="campaign-engagements">
+                            0
+                        </div>
+                        <div className="campaign-shares">
+                            0
+                        </div>
+                        <div className="campaign-status">
+                            Active
+                        </div>
+                        <div className="campaign-end-date">{campaign.end_date.split("T")[0]}</div>
                         <div className="campaign-actions">
                             <div className="new-button">
                                 <div className="icon"><AssessmentOutlined /></div>
                                 <div className="title">View Report</div>
                             </div>
                         </div>
-                    </div>
-                    <div className="campaign-instance">
-                        <div className="basic-details">
-                            <div className="campaign-title">Instance of Campaign title</div>
-                            <div className="other-basic">
-                                <div className="influencer-basic">7 Influencers</div>
-                                <div className="brand-basic">Brand Company</div>
-                                <div className="time-posted">8:14PM</div>
-                            </div>
-                        </div>
-                        <div className="campaign-info">
-                            <div className="campaign-posts">
-                                <div className="post-value">16</div>
-                                <div className="post-title">posts</div>
-                            </div>
-                            <div className="campaign-views">
-                                <div className="view-value">1253</div>
-                                <div className="view-title">views</div>
-                            </div>
-                            <div className="campaign-engagement">
-                                <div className="engagement-value">93</div>
-                                <div className="engagement-title">engagements</div>
-                            </div>
-                            <div className="campaign-shares">
-                                <div className="share-value">12</div>
-                                <div className="share-title">shares</div>
-                            </div>
-                            <div className="campaign-status">
-                                Completed
-                            </div>
-                        </div>
-                        <div className="campaign-actions">
-                            <div className="new-button">
-                                <div className="icon"><AssessmentOutlined /></div>
-                                <div className="title">View Report</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="campaign-instance">
-                        <div className="basic-details">
-                            <div className="campaign-title">Instance of Campaign title</div>
-                            <div className="other-basic">
-                                <div className="influencer-basic">7 Influencers</div>
-                                <div className="brand-basic">Brand Company</div>
-                                <div className="time-posted">8:14PM</div>
-                            </div>
-                        </div>
-                        <div className="campaign-info">
-                            <div className="campaign-posts">
-                                <div className="post-value">16</div>
-                                <div className="post-title">posts</div>
-                            </div>
-                            <div className="campaign-views">
-                                <div className="view-value">1253</div>
-                                <div className="view-title">views</div>
-                            </div>
-                            <div className="campaign-engagement">
-                                <div className="engagement-value">93</div>
-                                <div className="engagement-title">engagements</div>
-                            </div>
-                            <div className="campaign-shares">
-                                <div className="share-value">12</div>
-                                <div className="share-title">shares</div>
-                            </div>
-                            <div className="campaign-status">
-                                Active
-                            </div>
-                        </div>
-                        <div className="campaign-actions">
-                            <div className="new-button">
-                                <div className="icon"><AssessmentOutlined /></div>
-                                <div className="title">View Report</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="campaign-instance">
-                        <div className="basic-details">
-                            <div className="campaign-title">Instance of Campaign title</div>
-                            <div className="other-basic">
-                                <div className="influencer-basic">7 Influencers</div>
-                                <div className="brand-basic">Brand Company</div>
-                                <div className="time-posted">8:14PM</div>
-                            </div>
-                        </div>
-                        <div className="campaign-info">
-                            <div className="campaign-posts">
-                                <div className="post-value">16</div>
-                                <div className="post-title">posts</div>
-                            </div>
-                            <div className="campaign-views">
-                                <div className="view-value">1253</div>
-                                <div className="view-title">views</div>
-                            </div>
-                            <div className="campaign-engagement">
-                                <div className="engagement-value">93</div>
-                                <div className="engagement-title">engagements</div>
-                            </div>
-                            <div className="campaign-shares">
-                                <div className="share-value">12</div>
-                                <div className="share-title">shares</div>
-                            </div>
-                            <div className="campaign-status">
-                                Completed
-                            </div>
-                        </div>
-                        <div className="campaign-actions">
-                            <div className="new-button">
-                                <div className="icon"><AssessmentOutlined /></div>
-                                <div className="title">View Report</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="campaign-instance">
-                        <div className="basic-details">
-                            <div className="campaign-title">Instance of Campaign title</div>
-                            <div className="other-basic">
-                                <div className="influencer-basic">7 Influencers</div>
-                                <div className="brand-basic">Brand Company</div>
-                                <div className="time-posted">8:14PM</div>
-                            </div>
-                        </div>
-                        <div className="campaign-info">
-                            <div className="campaign-posts">
-                                <div className="post-value">16</div>
-                                <div className="post-title">posts</div>
-                            </div>
-                            <div className="campaign-views">
-                                <div className="view-value">1253</div>
-                                <div className="view-title">views</div>
-                            </div>
-                            <div className="campaign-engagement">
-                                <div className="engagement-value">93</div>
-                                <div className="engagement-title">engagements</div>
-                            </div>
-                            <div className="campaign-shares">
-                                <div className="share-value">12</div>
-                                <div className="share-title">shares</div>
-                            </div>
-                            <div className="campaign-status">
-                                Inactive
-                            </div>
-                        </div>
-                        <div className="campaign-actions">
-                            <div className="new-button">
-                                <div className="icon"><AssessmentOutlined /></div>
-                                <div className="title">View Report</div>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+                    ))}
                 </div>
 {/*                 <div className="filter-section">
                     <div className="top">
--- src/components/campaigns/New.jsx
+++ src/components/campaigns/New.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { saveCampaign } from '../../actions/campaign'
 import Sidebar from '../partials/sidebar/Sidebar'
 
 import './new.scss'
 
 const New = () => {
+  let dispatch = useDispatch()
+  let navigate = useNavigate()
+/*   const [values, setValues] = useState({
+    title: '',
+    brand: '',
+    category: '',
+    hastags: '',
+    description: '',
+    budget: '',
+    engagements: '',
+    views: '',
+    audience: '',
+    goal: '',
+    end_date: ''
+  }) */
+
+  let handleCampaignCreate = async(e) => {
+    e.preventDefault()
+    let title = document.querySelector('#campaign-title').value
+    let brand = document.querySelector('#campaign-brand').value
+    let category = document.querySelector('#campaign-category').value
+    let hashtags = document.querySelector('#hashtags').value
+    let description = document.querySelector('#description').value
+    let budget = document.querySelector('#campaign-budget').value
+    let engagements = document.querySelector('#campaign-engagements').value
+    let audience = document.querySelector('#campaign-audience').value
+    let views = document.querySelector('#campaign-views').value
+    let end_date = document.querySelector('#campaign-end').value
+    let goal = document.querySelector('#campaign-goal').value
+
+    await dispatch(saveCampaign({title, brand, category, hashtags, description, budget, engagements, audience, views, end_date, goal}))
+    navigate('/campaigns')
+  }
   return (
     <div>
         <Sidebar />
@@ -30,8 +66,8 @@ const New = () => {
                       <label htmlFor="campaign-brand" className='create-label'>Brand</label>
                     </div>
                     <div className="formInput" style={{width: '47%'}}>
-                      <input type="text" className='create-input' id='campaign-brand' placeholder=' ' style={{padding: '7%'}} />
-                      <label htmlFor="campaign-brand" className='create-label'>Category</label>
+                      <input type="text" className='create-input' id='campaign-category' placeholder=' ' style={{padding: '7%'}} />
+                      <label htmlFor="campaign-category" className='create-label'>Category</label>
                     </div>
                   </form>
                 </div>
@@ -48,7 +84,7 @@ const New = () => {
                     <label htmlFor='hashtags' className='create-label'>Hashtags and Mentions</label>
                   </div>
                   <div className="formInput" style={{width: '100%'}}>
-                    <textarea name="" className='create-input' id="" cols="30" rows="10" placeholder=' ' style={{padding: '2%'}}></textarea>
+                    <textarea name="" className='create-input' id="description" cols="30" rows="10" placeholder=' ' style={{padding: '2%'}}></textarea>
                     <label htmlFor='description' className='create-label'>Description</label>
                   </div>
                   </form>
@@ -77,11 +113,19 @@ const New = () => {
                       <input type="number" className="create-input" id="campaign-audience" placeholder=' ' style={{padding: '3%'}} />
                       <label htmlFor="campaign-audience" className="create-label">Audience Target</label>
                     </div>
+                    <div className="formInput" style={{width: '47%'}}>
+                      <input type="text" className="create-input" name='campaign-goal' id="campaign-goal" placeholder=' ' style={{padding: '7%'}} />
+                      <label htmlFor="campaign-goal" className="create-label">Goal</label>
+                    </div>
+                    <div className="formInput" style={{width: '47%'}}>
+                      <input type="date" className="create-input" name='campaign-end' id="campaign-end" placeholder=' ' style={{padding: '7%'}} />
+                      <label htmlFor="campaign-end" className="create-label">Completion Target</label>
+                    </div>
                   </form>
                 </div>
               </div>
               <div className="button-section">
-                <button>Create</button>
+                <button onClick={handleCampaignCreate}>Create</button>
               </div>
           </div>
         </div>
