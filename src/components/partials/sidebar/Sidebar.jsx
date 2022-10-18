0ca06ee46f63316cfb40e09fc4284957f3e38292
vreemdeling <bamgbade_tunde@yahoo.com>
2022-10-18 20:51:06 +0100

    front-end changes

--- src/components/partials/sidebar/Sidebar.jsx
+++ src/components/partials/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link, useNavigate } from "react-router-dom"
+import decode from 'jwt-decode'
 
 import { AnalyticsOutlined, CampaignOutlined, DraftsOutlined, LogoutOutlined, PersonSearchOutlined } from "@mui/icons-material"
 
