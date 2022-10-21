33a5b5e9044942b6d368c5abdd71ad1eabedbe1b
vreemdeling <bamgbade_tunde@yahoo.com>
2022-10-21 04:27:55 +0100

    front-end changes

--- src/actions/influencer.js
+++ src/actions/influencer.js
@@ -1,5 +1,6 @@
 import { INFLUENCER_FETCH, INFLUENCER_SAVE } from "../constants/types"
 import * as api from '../api'
+import { PhylloConnectSDK } from "../phyllo/initialize-phyllo"
 
 export const fetchInfluencers  = () => async(dispatch) => {
     try {
@@ -16,6 +17,9 @@ export const connectSDKAction = (id) => async(dispatch) => {
         console.log(id)
         let response = await api.connectSDKAction({id})
         console.log(response)
+        let config = {env: 'sandbox', userId: response.id, token: response.token, appName: "Kaylaa", redirectURL: window.location.href }
+        const phyllo = PhylloConnectSDK.initialize(config)
+        phyllo.open()
     } catch (error) {
         console.log(error.body)
     }
